5a511bd788e875c0b24ceecf277bd5ea95119604
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-05-30 18:31:33 -0400

    Part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      students: []
+    };
+    // console.log('App constructor');
+  };
+
+  componentDidMount() {
+    fetch('https://api.hatchways.io/assessment/students')
+      .then((response) => response.json())
+      .then((user) => this.setState(() => {
+        return { students: user}
+      }, () => {console.log(this.state)}
+    ));
+  };
+
+  // average = student.grades.map((student) => {
+  //   return student.grade;
+  // )};
+  
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Student Profiles</h1>
+        <div>
+          {this.state.students.students?.map((student) => {
+            const avg =
+            student.grades.reduce((sum, curr) => sum + Number(curr), 0) /
+            student.grades.length;
+
+
+            return (
+              <div key={student.id}>
+                <img src={student.pic}/>
+                <h2>{student.firstName} {student.lastName}</h2>
+                <p>{student.email}</p>
+                <p>{student.company}</p>
+                <p>{student.skill}</p>
+                <p>{avg}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
